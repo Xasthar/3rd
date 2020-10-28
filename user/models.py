ec708ba0df2d639034386dbfd9c3c9bc7abccfc7
Xasthar <luksor1_1991@o2.pl>
2020-10-28 21:41:30 +0000

    Started from login system

---
+++ user/models.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, jsonify
+
+class User:
+
+    def signup(self):
+
+        user = {
+            "_id": "",
+            "name": "",
+            "email": "",
+            "password": ""
+        }
+    
+    return jsonify(user), 200
---
+++ user/routes.py
@@ -0,0 +1,8 @@
+import os
+from flask import Flask
+from app import app
+from user.models import User
+
+@app.route('/user/signup', methods=['GET'])
+def signup():
+    return User().signup()
